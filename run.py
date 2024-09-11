1117b0ef02c75dd7896311e350686d4fe5fe03d3
Vinícius Lameirão <viniciuslameirao@hotmail.com>
2024-09-11 02:30:04 -0300

    fix run.py

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from scrap import KabumScrap, AmazonScrap
 link = str(input("URL: "))
 output = str(input("File name output: "))
 
-if("kabum".lower() in link):
-    print("kabum")
+if("kabum" in link.lower()):
+    print("Scraping Kabum")
     kabum = KabumScrap(link)
     kabum.getProducts()
     kabum.writeOutput(output)
 else:
-    print("amazon")
+    print("Scraping Amazon")
     amazon = AmazonScrap(link)
     amazon.getProducts()
     amazon.writeOutput(output)
